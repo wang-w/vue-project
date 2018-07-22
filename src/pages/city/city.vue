2730712f51052446d069bad22e139a1d20e3ba66
wang-w <342277176@qq.com>
2018-07-22 20:21:06 +0800

    city-router开发

---
+++ src/pages/city/city.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <city-header></city-header>
+  </div>
+</template>
+
+<script>
+import cityHeader from './components/Header'
+export default {
+  name: 'city',
+  components: {
+    cityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe60b;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .header
+    position relative
+    height $headerHeightrem
+    line-height $headerHeight
+    overflow hidden
+    text-align center
+    font-size .4rem
+    color #fff
+    background $bgColor
+    .header-back
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      text-align center
+      font-size .4rem
+      color #fff
+</style>
