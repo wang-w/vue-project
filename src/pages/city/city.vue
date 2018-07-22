e17e8532144239498242a73baf2ca59364573cb7
wang-w <342277176@qq.com>
2018-07-22 21:40:08 +0800

    city-list开发

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,17 +2,23 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import cityHeader from './components/Header'
 import citySearch from './components/Search'
+import cityList from './components/List'
+import cityAlphabet from './components/Alphabet'
 export default {
   name: 'city',
   components: {
     cityHeader,
-    citySearch
+    citySearch,
+    cityList,
+    cityAlphabet
   }
 }
 </script>
