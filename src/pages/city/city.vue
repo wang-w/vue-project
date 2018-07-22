6aaa62c95715b3f134c36413706aa70aa67a7e52
wang-w <342277176@qq.com>
2018-07-22 20:36:42 +0800

    city-input开发

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import cityHeader from './components/Header'
+import citySearch from './components/Search'
 export default {
   name: 'city',
   components: {
-    cityHeader
+    cityHeader,
+    citySearch
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input class="serach-input" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" coped>
+  @import "~styles/varibles.styl"
+  .search
+    height .72rem
+    padding 0 .1rem
+    background $bgColor
+    .serach-input
+      width 100%
+      height .62rem
+      padding 0 .1rem
+      box-sizing border-box
+      line-height .62rem
+      text-align center
+      border-radius .06rem
+      color #666
+      font-family "Microsoft Yahei"
+</style>
